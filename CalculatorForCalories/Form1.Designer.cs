eee4fd090cfd9e3abbe8411f242bee216e0914cc
Callaaa <privatmf@abv.bg>
2024-05-27 23:14:29 +0300

    Calculator.Calories

---
+++ CalculatorForCalories/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CalculatorForCalories
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmbSports = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Време в минути";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(275, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Спорт";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(460, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Резултат";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(436, 102);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(155, 34);
+            this.txtResult.TabIndex = 3;
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinutes.Location = new System.Drawing.Point(21, 102);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(155, 34);
+            this.txtMinutes.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalculate.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(122, 195);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(142, 76);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Изчисли";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(359, 195);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(142, 76);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Изчисти";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmbSports
+            // 
+            this.cmbSports.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmbSports.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSports.FormattingEnabled = true;
+            this.cmbSports.Items.AddRange(new object[] {
+            "Аеробика",
+            "Боулинг",
+            "Волейбол",
+            "Тенис на корт",
+            "Футбол",
+            "Ски",
+            "Баскетбол"});
+            this.cmbSports.Location = new System.Drawing.Point(240, 102);
+            this.cmbSports.Name = "cmbSports";
+            this.cmbSports.Size = new System.Drawing.Size(155, 34);
+            this.cmbSports.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 345);
+            this.Controls.Add(this.cmbSports);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Калкyлатор за калории";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cmbSports;
+    }
+}
+
---
+++ CalculatorForCalories/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorForCalories
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double minuti = int.Parse(txtMinutes.Text);
+                double resultat = 0;
+                switch (cmbSports.SelectedItem)
+                {
+                    case "Аеробика":
+                        resultat = Math.Round((500 / 60) * minuti);
+                        break;
+                    case "Боулинг":
+                        resultat = Math.Round((200 / 60) * minuti);
+                        break;
+                    case "Волейбол":
+                        resultat = Math.Round((220 / 60) * minuti);
+                        break;
+                    case "Тенис на корт":
+                        resultat = Math.Round((540 / 60) * minuti);
+                        break;
+                    case "Футбол":
+                        resultat = Math.Round((420 / 60) * minuti);
+                        break;
+                    case "Ски":
+                        resultat = Math.Round((300 / 60) * minuti);
+                        break;
+                    case "Баскетбол":
+                        resultat = Math.Round((570 / 60) * minuti);
+                        break;
+                }
+
+                if (cmbSports.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Невъведени данни!", "Грешка!",
+                        MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    txtResult.Text = resultat.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невъведени данни!", "Грешка!",
+                   MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+            txtMinutes.Clear();
+            cmbSports.SelectedIndex = -1;
+        }
+    }
+}
